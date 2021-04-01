385ee3021def6869bf2d22c4c51c36c3dbad99ce
MrRayBen <40451847+MrRayBen@users.noreply.github.com>
2021-04-01 18:38:00 +0200

    main

---
+++ Start.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+
+echo "Select your Linux Distro:"
+echo " 1-> Ubuntu Based 2 -> Arch "
+
+
+menu_option_one(){
+    echo "You Selected Ubuntu Based!"
+    echo "1. Update System"
+    echo "2. list apps"
+
+
+menu_option_one(){
+    sudo apt update && sudo apt upgrade
+
+
+
+}
+
+
+
+}
+
+menu_option_two(){
+    echo "You Selected Arch Based!"
+}
